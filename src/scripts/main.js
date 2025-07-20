941e7f89f9a32e8bf21ddffe9190dc5e6b1de7b9
joãoA <joaovictor.dudu03@gmail.com>
2025-07-20 01:27:01 -0300

    projeto finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,3 @@
-// document.addEventListener('DOMContentLoaded',function(){
-
-//     const form = document.getElementById('form')
-//     const successMessage = document.getElementById('successMessage')
-//     const userEmail = document.getElementById('userEmail')
-//     const formSection = document.getElementById('formSection')
-//     const dismissBtn = document.getElementById('dismissBtn')
-//     const spanError = document.getElementById('spanError')
-//     const inputEmail = document.getElementById('inputEmail')
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-//     form.addEventListener('submit', function(e){
-//         e.preventDefault()
-//         if(inputEmail.value === '' || !emailRegex.test(inputEmail.value)){
-//             inputEmail.classList.add('input-error')
-//             spanError.classList.add('span-error')
-//             spanError.style.display = 'block'
-//             inputEmail.focus()
-//         }
-//         else{
-//             inputEmail.classList.remove('input-error')
-//             spanError.classList.remove('span-error')
-//             spanError.style.display = 'none'
-
-//             userEmail.textContent = inputEmail.value
-//             formSection.style.display = 'none'
-//             successMessage.style.display = 'block'
-//         }
-//     })
-
-//     dismissBtn.addEventListener('click', function(){
-//         formSection.style.display = 'block'
-//         successMessage.style.display = 'none'
-//         form.reset()
-//     })
-// })
-
-
 class EmailForm {
   constructor(formSelector) {
     this.form = document.querySelector(formSelector);
@@ -97,6 +59,44 @@ class EmailForm {
 document.addEventListener('DOMContentLoaded', () => {
   new EmailForm('#form');
 });
+
+
+// document.addEventListener('DOMContentLoaded',function(){
+
+//     const form = document.getElementById('form')
+//     const successMessage = document.getElementById('successMessage')
+//     const userEmail = document.getElementById('userEmail')
+//     const formSection = document.getElementById('formSection')
+//     const dismissBtn = document.getElementById('dismissBtn')
+//     const spanError = document.getElementById('spanError')
+//     const inputEmail = document.getElementById('inputEmail')
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+//     form.addEventListener('submit', function(e){
+//         e.preventDefault()
+//         if(inputEmail.value === '' || !emailRegex.test(inputEmail.value)){
+//             inputEmail.classList.add('input-error')
+//             spanError.classList.add('span-error')
+//             spanError.style.display = 'block'
+//             inputEmail.focus()
+//         }
+//         else{
+//             inputEmail.classList.remove('input-error')
+//             spanError.classList.remove('span-error')
+//             spanError.style.display = 'none'
+
+//             userEmail.textContent = inputEmail.value
+//             formSection.style.display = 'none'
+//             successMessage.style.display = 'block'
+//         }
+//     })
+
+//     dismissBtn.addEventListener('click', function(){
+//         formSection.style.display = 'block'
+//         successMessage.style.display = 'none'
+//         form.reset()
+//     })
+// })
 
 
 
